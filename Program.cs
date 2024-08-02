ee714f34ff15d2505840759dd4c4d783d00a2cd2
yazanMhussein <yazan.m.hussein@gmail.com>
2024-08-02 17:25:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalFlixERD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
